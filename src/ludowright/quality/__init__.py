2cff667411936635df93239b32ef1b7f70ae400c
Raillen Santos <50306048+raillen@users.noreply.github.com>
2026-07-29 11:23:41 -0300

    Add quality package

---
+++ src/ludowright/quality/__init__.py
@@ -0,0 +1,17 @@
+"""Engineering quality checks for LudoWright."""
+
+from ludowright.quality.checks import (
+    QUALITY_CHECKS,
+    RELEASE_CHECKS,
+    CheckResult,
+    CheckSpec,
+    run_checks,
+)
+
+__all__ = [
+    "QUALITY_CHECKS",
+    "RELEASE_CHECKS",
+    "CheckResult",
+    "CheckSpec",
+    "run_checks",
+]
